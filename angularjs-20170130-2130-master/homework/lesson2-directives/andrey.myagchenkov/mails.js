13eb1561475b77b53f06ad86405aa17fc24865da
Stepan Suvorov <stevermeister@gmail.com>
2017-02-09 22:21:03 +0200

    cleanup

--- angularjs-20170130-2130-master/homework/lesson2-directives/andrey.myagchenkov/mails.js
+++
@@ -1,6 +0,0 @@
-let mailsFromServer = [ {"date": new Date(2017, 0, 2), "from": "robert@gmail.com", "text": "Robert's mail. Something text for Angular"},
-						{"date": new Date(2016, 11, 15), "from": "john@gmail.com", "text": "John's mail. Something text for Robert"},
-						{"date": new Date(2017, 1, 0), "from": "kate@gmail.com", "text": "Kate's mail. Something text for John"},
-					 	{"date": new Date(2017, 1, 6, 18, 5, 17), "from": "Olga@gmail.com", "text": "Olga's mail. Something text for Kate"},
-					 	{"date": new Date(2017, 1, 7, 16, 3, 14), "from": "angular@gmail.com", "text": "Angular's mail. Something text for Olga"},
-				   	  ];
