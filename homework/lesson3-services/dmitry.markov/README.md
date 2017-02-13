f3a9992c212024b8ca085055f1d673d7ef5ab8cc
DmitryMarkov <forall@stylive.com>
2017-02-13 16:50:14 +0200

    homework bootstrap

---
+++ homework/lesson3-services/dmitry.markov/README.md
@@ -0,0 +1,34 @@
+# Домашняя работа №3 Дмитрий Марков
+
+### Задание
+
+#### Приложение "конвертер валют"
+
+1. Сделать виджет компонент "конвертер валют" CurrencyConverter
+2. Написать сервис CurrencyConverterSerivce, с логикой конвертации.
+3. Подключить сервис для получения курсов валют (http запрос)
+
+#### Приложение "Список пользователей"
+
+1. Написать сервис UserService для получения пользователей по http(https://learn.javascript.ru/courses/groups/api/participants?key=1gvlw0r)
+2. Используя сервис из (1) вывести список.
+
+#### Бонус:
+1. реализовать свою директиву ng-show, ng-hide, ng-click, ng-change (на чистом javascript, без использования Angular)
+
+### Demo
+http://plnkr.co/edit/aK1w6SSsBLX1LbJuBxDq?p=preview
+
+### TODO:
+- [ ] currency widget component
+- [ ] currency widget service
+- [ ] currency widget http api
+- [ ] currency widget history
+- [ ] currency widget choose date
+- [ ] currency widget proper currency names (+plural)
+- [ ] userService http service
+- [ ] userService list component
+- [ ] nrg-show
+- [ ] nrg-hide
+- [ ] nrg-click
+- [ ] nrg-change
---
+++ homework/lesson3-services/dmitry.markov/script.js
@@ -0,0 +1,67 @@
+// jshint asi: true
+;(function () {
+  'use strict'
+
+  angular.module('currencyWidget', ['ngAnimate', 'toastr'])
+  let app = angular.module('currencyWidget')
+
+  app.config((toastrConfig) => {
+    angular.extend(toastrConfig, {
+      positionClass: 'toast-bottom-center',
+      timeOut: 3500
+    })
+  })
+
+  app.component('appRoot', {
+    template: `
+      <div class="container">
+        <currency-widget></currency-widget>
+      </div>
+    `
+  })
+
+  app.component('currencyWidget', {
+    templateUrl: 'currency-widget.html',
+    controller (currencyService) {
+      currencyService.getRates()
+        .then((response) => {
+          this.data = response
+      })
+      currencyService.getNames()
+        .then((response) => {
+          this.names = response
+      })
+    }
+  })
+
+  app.factory('currencyService', function ($http, toastr) {
+
+    let ratesUrl = 'http://api.fixer.io/latest'
+    let namesUrl = 'https://gist.githubusercontent.com/Fluidbyte/2973986/raw/b0d1722b04b0a737aade2ce6e055263625a0b435/Common-Currency.json'
+
+    function getRates () {
+      return $http.get(ratesUrl)
+          .then((response) => {
+            return response.data
+          }, (error) => {
+            toastr.info(error)
+          })
+    }
+
+    function getNames () {
+      return $http.get(namesUrl)
+          .then((response) => {
+            return response.data
+          }, (error) => {
+            toastr.info(error)
+          })
+    }
+
+    let service = {
+      getRates,
+      getNames
+    }
+
+    return service
+  })
+})()
